014314707ab5d3e130353646b1fbb1a2d1134e09
deekshanambi <deekshanambi@gmail.com>
2026-03-02 11:32:37 +0530

    Update AppTest.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,10 +1,31 @@
 package com.example;
+
 import org.junit.Test;
 import static org.junit.Assert.*;
+
 public class AppTest {
- @Test
- public void testAdd() {
- App app = new App();
- assertEquals(5, app.add(2, 3));
- }
+
+    @Test
+    public void testAdd() {
+        App calculator = new App();
+        assertEquals(15, calculator.add(10, 5));
+    }
+
+    @Test
+    public void testSubtract() {
+        App calculator = new App();
+        assertEquals(5, calculator.subtract(10, 5));
+    }
+
+    @Test
+    public void testMultiply() {
+        App calculator = new App();
+        assertEquals(50, calculator.multiply(10, 5));
+    }
+
+    @Test
+    public void testDivide() {
+        App calculator = new App();
+        assertEquals(2, calculator.divide(10, 5));
+    }
 }
